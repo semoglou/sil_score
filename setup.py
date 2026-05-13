e7bfaa2c6e5f4e02c7792499e2328d2f835a402d
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-14 00:03:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sil-score",
-    version="0.1.0",
+    version="0.1.2",
     description="Exact and approximate silhouette scoring with micro, macro, and weighted cluster averages.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
