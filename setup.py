b49dd25fcf5ec7012462a74b7048fb3fa98ac09b
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-14 00:16:04 +0300

    Bump version from 0.1.4 to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sil-score",
-    version="0.1.4",
+    version="0.1.5",
     description="Exact and approximate silhouette scoring with micro, macro, and weighted cluster averages.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
